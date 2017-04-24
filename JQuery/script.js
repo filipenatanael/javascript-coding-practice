4d2315e4990f83c0b3f214313319fc0ae11bad97
Filipe Natanael <filipenatanael@live.com>
2017-04-23 22:12:48 -0300

    Get and Post requisition
    Get and Post requisition

--- JQuery/script.js
+++ JQuery/script.js
@@ -1,7 +1,37 @@
+
+/*
+REQUISITION BY POST
+
 $(function(){
-  //Especificado pelo ID(#) do elemento: $('#ID_Elemento').bind('click', function(){
-  //Especificado pelo elemento: $('Elemento').bind('click', function(){
+  $('button').bind('click', function(){
+    $.post("testing.html", function(result){
+        $('.div').html(result);
+    });
+  });
+});
+*/
+
+/*
+REQUISITION BY GET
+
+$(function(){
+  $('button').bind('click', function(){
+    $.get("testing.html", function(result){
+        $('.div').html(result);
+    });
+  });
+});
+
+*/
+
+/*
+Especificado pelo ID(#) do elemento: $('#ID_Elemento').bind('click', function(){
+Especificado pelo elemento: $('Elemento').bind('click', function(){
+
+$(function(){
   $('button').bind('click', function(){
     $('.div').load("testing.html");
   });
 });
+
+*/
