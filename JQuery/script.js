dd5efc1a1314fcf9389b1ca6b9e841c8d4b6a238
Filipe Natanael <filipenatanael@live.com>
2017-04-23 21:47:38 -0300

    ...
    ...

---
+++ JQuery/script.js
@@ -0,0 +1,7 @@
+$(function(){
+  //ID: $('#ID_ELEMENTO').bind('click', function(){
+  //Elemento: $('div').bind('click', function(){
+  $('div').bind('click', function(){
+    $('.div').load("testing.html");
+  });
+});
