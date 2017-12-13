4c647adabe8effb6ab76cf3db00d91b031069a53
Filipe Natanael <filipenatanael@live.com>
2017-12-12 23:27:18 -0200

    New Methods for Strings
    New Methods for Strings

--- JS-TDD-Course/Learning/05-ShorthandProperties.js
+++ JS-TDD-Course/Learning/05-ShorthandProperties.js
@@ -1,12 +1,17 @@
+/* Shorthand Properties - Using ES6 */
 let firstName = 'John';
 let lastName = 'Brubaker';
 
-/* Using ES6 */
 const person = {
   firstName,
   lastName
 }
-
-console.log(person);
-
 /* Using ES5 const person = {'firstName': firstName,'lastName': lastName } */
+
+/* New Methods for Strings */
+let text = 'My text testing!';
+
+console.log(text.startsWith('My', 2));
+console.log(text.endsWith('ing!', 16));
+console.log('test\n'.repeat(3));
+console.log(text.includes('testin'));
