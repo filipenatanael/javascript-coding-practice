63e785a4a9308106396446f9452818e4ce436a23
Filipe Natanael <filipenatanael@live.com>
2017-12-12 23:16:17 -0200

    Shorthand Properties
    Shorthand Properties

---
+++ JS-TDD-Course/Learning/05-ShorthandProperties.js
@@ -0,0 +1,12 @@
+let firstName = 'John';
+let lastName = 'Brubaker';
+
+/* Using ES6 */
+const person = {
+  firstName,
+  lastName
+}
+
+console.log(person);
+
+/* Using ES5 const person = {'firstName': firstName,'lastName': lastName } */
