215d959b72dae040e8c32bef46e0de07d1a29610
Filipe Natanael <filipenatanael@live.com>
2017-12-24 23:57:38 -0200

    Function and Arrow Function Declaration
    Using Function and Arrow Function Declaration

--- JS-TDD-Course/Learning/ArrayFunctions.js
+++ JS-TDD-Course/Learning/ArrayFunctions.js
@@ -14,8 +14,20 @@ const data = [
 const numbers = [5, 7, 2, 0, -1, -7];
 
 console.log('Using find(value): '+numbers.find(x => x < 0));
-
 console.log('Using findIndex: '+numbers.findIndex(x => x < 0));
 
 const John = data.find(john => john.name === 'John')
 console.log(John);
+
+/* Using function declaration */
+const Ex1_John = numbers.find(function isBigEnough(element) {
+  return element <= 2;
+})
+console.log(Ex1_John);
+
+
+/* Using Arrow function declaration */
+const Ex2_John = numbers.find(
+  value => value <= 2
+)
+console.log(Ex2_John);
