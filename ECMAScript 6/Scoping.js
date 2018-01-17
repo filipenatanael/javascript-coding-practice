7a1fdf1ee37bb6ed1c764632a5d6a5537f67557a
Filipe Natanael <filipenatanael@live.com>
2018-01-16 23:56:21 -0200

    [Scoping] Block-Scoped Variables
    [Scoping] Block-Scoped Variables

---
+++ ECMAScript 6/Scoping.js
@@ -0,0 +1,12 @@
+/*
+Reviewing the study - Block-Scoped Variables
+*/
+
+for (let i = 0; i < 10; i++)
+{
+  console.log(i);
+}
+/*
+console.log(i);
+Return error! because the variable (i) it's ouside of scope
+*/
