e05a8d88cd5fd10a75fbe7e2cdf0dd7d59a38e0f
Filipe Natanael <filipenatanael@live.com>
2018-01-17 00:17:41 -0200

    [Scoping] Block-Scoped Functions
    [Scoping] Block-Scoped Functions

--- ECMAScript 6/Scoping.js
+++ ECMAScript 6/Scoping.js
@@ -1,12 +1,35 @@
-/*
-Reviewing the study - Block-Scoped Variables
-*/
-
+/* Reviewing the study - Block-Scoped Variables */
 for (let i = 0; i < 10; i++)
 {
   console.log(i);
 }
 /*
 console.log(i);
-Return error! because the variable (i) it's ouside of scope
+Return error! because the variable (i) it's ouside of scope.
 */
+
+
+/* Block-Scoped Functions with Arrow Functions */
+{
+  const Hello = () => { return 'Hello Everyone!'; }
+  console.log(Hello());
+    {
+      const Hello = () => { return 'Hello There!'; }
+    }
+  console.log(Hello());
+}
+/*
+console.log(Hello());
+Return error! because the function it's ouside of scope.
+*/
+
+
+/* Block-Scoped Functions with wirte default function */
+{
+  function Hello(){ return 0 }
+  Hello()
+    {
+    function Hello(){ return 1 }
+    }
+  Hello()
+}
