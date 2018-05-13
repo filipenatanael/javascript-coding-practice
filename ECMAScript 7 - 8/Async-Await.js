b8fa2979a2080daff2dd52df1948c03f736787d3
Filipe Natanael <filipenatanael@live.com>
2018-05-12 23:57:31 -0300

    Rewrite using async/await

--- ECMAScript 7 - 8/Async-Await.js
+++ ECMAScript 7 - 8/Async-Await.js
@@ -1,15 +1,24 @@
-//Async Await Example
+// Async Await Example
+// Simplified code with less chaining and facility to debug
 
-function getMyIp() {
+async function getMyIp() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json();
 
-  
+  data.map(post => {
+    console.log(post.title);
+  });
 
-  //ECMAScript 6
-  // return fetch('https://jsonplaceholder.typicode.com/posts')
-  //       .then(data => data.json())
-  //       .then(data => data.map(post => {
-  //         console.log(post.title);
-  //       }));
 }
+
+// ECMAScript 6
+//
+// function getMyIp() {
+//   return fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(data => data.json())
+//   .then(data => data.map(post => {
+//     console.log(post.title);
+//   }));
+// }
 
 getMyIp();
