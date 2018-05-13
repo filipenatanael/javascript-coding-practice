cdfea578d81e03ccd9cbecf4d271c208c7c6b627
Filipe Natanael <filipenatanael@live.com>
2018-05-12 23:49:16 -0300

    ECMAScript 7 - 8

---
+++ ECMAScript 7 - 8/Async-Await.js
@@ -0,0 +1,15 @@
+//Async Await Example
+
+function getMyIp() {
+
+  
+
+  //ECMAScript 6
+  // return fetch('https://jsonplaceholder.typicode.com/posts')
+  //       .then(data => data.json())
+  //       .then(data => data.map(post => {
+  //         console.log(post.title);
+  //       }));
+}
+
+getMyIp();
