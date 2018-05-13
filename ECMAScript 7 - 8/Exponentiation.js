e91a41d0f7f911bf62bd9fb3a3c02df9ed29da56
Filipe Natanael <filipenatanael@live.com>
2018-05-13 00:42:55 -0300

    Using Exponentiation

---
+++ ECMAScript 7 - 8/Exponentiation.js
@@ -0,0 +1,7 @@
+// Exponentiation
+
+// [ Tradicional ] Return the value of the number 4 to the power of 3 (5*5*5):
+console.log(Math.pow(5, 3));
+
+// [ Using Exponentiation ]
+console.log(5 ** 3);
