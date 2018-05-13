3810e5dc92dfa8c4752ff4c3bc6e335fff91629a
Filipe Natanael <filipenatanael@live.com>
2018-05-13 01:24:07 -0300

    Trailing commas in functions

---
+++ ECMAScript 7 - 8/Trailing-Commas.js
@@ -0,0 +1,20 @@
+// Trailing Commas
+//
+// Arrays
+var arr = [
+  1,
+  2,
+  3,
+];
+
+// Objects
+var object = {
+  name: "John",
+  email: "john@example.com",
+  age: 21,
+};
+
+// Functions
+function getNumbers(num1, num2,) {
+  return num1 + num2;
+}
