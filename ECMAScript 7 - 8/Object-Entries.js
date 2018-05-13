d1527ca37869e443e8714e76a3069c9eea104061
Filipe Natanael <filipenatanael@live.com>
2018-05-13 01:03:57 -0300

    [Object entries] - Return key and values

---
+++ ECMAScript 7 - 8/Object-Entries.js
@@ -0,0 +1,16 @@
+// Object.entries() return [key, value]
+
+const countries = {
+  Brazil: 1,
+  Uruguay: 2,
+  Germany: 3
+}
+
+const countriesMap = new Map(Object.entries(countries));
+
+console.log(countriesMap.size);
+console.log(countriesMap.has('Uruguay'));
+console.log(countriesMap.get('Brazil'));
+
+// Using Object.keys(obj) return [key] only!
+console.log(countriesMap);
