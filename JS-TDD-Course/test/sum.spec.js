ae7abaf23477d254dda40fccd5f135d4a018c1ed
Filipe Natanael <filipenatanael@live.com>
2018-07-06 01:44:17 -0300

    Testing Commit

---
+++ JS-TDD-Course/test/sum.spec.js
@@ -0,0 +1,15 @@
+const { expect } = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+const { sum } = require('../Learning/Sum');
+
+lab.test('returns true when 1 + 1 equals 2', () => {
+    let x = 1;
+    let y = 1;
+    expect(sum(x, y)).to.equal(2);
+});
+
+lab.test('Run function without params', () => {
+    expect(sum()).to.equal(0);
+});
