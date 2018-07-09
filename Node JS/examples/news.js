a116c3626202c605cc0ac0f939e57b9152a1b435
Filipe Natanael <filipenatanael@live.com>
2018-07-09 01:26:38 -0300

    Responding requests based on requested URL

--- Node JS/examples/news.js
+++ Node JS/examples/news.js
@@ -1,5 +1,18 @@
 var http = require('http');
 
 http.createServer(function (req, res){
-  res.end("News Portal!");
+
+  let category =  req.url;
+
+  switch (category) {
+    case '/technology':
+    res.end("<b>News about technology!<b>");
+    break;
+    case '/science':
+    res.end("<b>News about science!<b>");
+    break;
+    default:
+    res.end("<b>Home Portal!<b>")
+  }
+
 }).listen(3000);
