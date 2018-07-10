4526229894b598d27bcb1c4bf14674d95bcdcb2e
Filipe Natanael <filipenatanael@live.com>
2018-07-09 23:15:05 -0300

    Set express engine to use EJS

--- Node JS/app.js
+++ Node JS/app.js
@@ -1,12 +1,16 @@
 var express = require('express'); // Return a function, i can use also require('express')();
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('<b>Home Portal!<b>')
-});
+// Set express engine to use EJS
+app.set('view engine', 'ejs');
 
 app.get('/technology', function(req, res){
-  res.send('<b>News about technology!<b>')
+  res.render('session/technology');
+});
+
+
+app.get('/', function(req, res){
+  res.send('<b>Home Portal!<b>');
 });
 
 app.listen(3000, function(){
