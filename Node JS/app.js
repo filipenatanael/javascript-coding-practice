d2ac40ed948c29ab2f6fa411d38e40dbd1438428
Filipe Natanael <filipenatanael@live.com>
2018-07-09 23:36:22 -0300

    Install Nodemon Utility

--- Node JS/app.js
+++ Node JS/app.js
@@ -4,13 +4,25 @@ var app = express();
 // Set express engine to use EJS
 app.set('view engine', 'ejs');
 
-app.get('/technology', function(req, res){
-  res.render('session/technology');
-});
-
+// res.send similar to res.end
+// app.get('/', function(req, res){
+//   res.send('<b>Home Portal!<b>');
+// });
 
 app.get('/', function(req, res){
-  res.send('<b>Home Portal!<b>');
+  res.render('home/index');
+});
+
+app.get('/form-add-news', function(req, res){
+  res.render('admin/form-add-news');
+});
+
+app.get('/news', function(req, res){
+  res.render('news/news');
+});
+
+app.get('/technology', function(req, res){
+  res.render('session/technology');
 });
 
 app.listen(3000, function(){
