996d0f075b114172712a9446f9c4e7942bbbb7d9
Filipe Natanael <filipenatanael@live.com>
2018-07-17 00:33:34 -0300

    Refactoring the method of connecting

--- Node JS/app/routes/news.js
+++ Node JS/app/routes/news.js
@@ -1,14 +1,9 @@
+var Connection = require('../../config/database');
+
 module.exports = function(app) {
   app.get('/news', function(req, res){
 
-    var mysql = require('mysql');
-
-    var connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'toor',
-      database: 'node_js_pratice'
-    });
+    var connection = Connection();
 
     connection.query('select * from news', function(error, result){
       res.render('news/news', { news: result });
