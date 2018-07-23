564318eaa12fda6e8fbe10b801a616e8ef5132ec
Filipe Natanael <filipenatanael@live.com>
2018-07-22 22:29:28 -0300

    Config Consign

--- Node JS/app.js
+++ Node JS/app.js
@@ -1,9 +1,9 @@
 var app = require('./config/server');
 
 // Require Routes
-var home = require('./app/routes/home')(app);
-var admin = require('./app/routes/admin')(app);
-var news = require('./app/routes/news')(app);
+// var home = require('./app/routes/home')(app);
+// var admin = require('./app/routes/admin')(app);
+// var news = require('./app/routes/news')(app);
 
 app.listen(3000, function(){
   console.log('Running with express.');
--- Node JS/config/server.js
+++ Node JS/config/server.js
@@ -1,6 +1,15 @@
 var express = require('express');
+var consign = require('consign');
+
 var app = express();
+
+/* Define constants */
+const VIEWS_DIRECTORY = './app/views';
+const ROUTES_DIRECTORY = './app/routes';
+
 app.set('view engine', 'ejs');  // Set express engine to use EJS
-app.set('views', './app/views') // Set default directory to express
+app.set('views', VIEWS_DIRECTORY) // Set default directory to express
+
+consign().include(ROUTES_DIRECTORY).into(app);
 
 module.exports = app;
