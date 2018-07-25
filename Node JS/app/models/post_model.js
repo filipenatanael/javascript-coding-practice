bb159fdde8d6f2e1a0f62b1953311f0feef14823
Filipe Natanael <filipenatanael@live.com>
2018-07-25 00:08:52 -0300

    34 - Finished Consign

--- Node JS/app/models/post_model.js
+++ Node JS/app/models/post_model.js
@@ -1,7 +1,12 @@
 module.exports = function() {
 
-  this.getPosts = function(connection, callback) {
-    connection.query('select * from news where id_news = 3', callback);
+  this.getPost = function(connection, callback) {
+    connection.query('select * from news where id_news = 3', callback)
   }
+
+  this.getPosts = function(connection, callback) {
+    connection.query('select * from news', callback)
+  }
+
   return this;
 }
--- Node JS/app/routes/post.js
+++ Node JS/app/routes/post.js
@@ -1,10 +1,10 @@
-module.exports = function(app) {
-  app.get('/post', function(req, res) {
+module.exports = function(application) {
+  application.get('/post', function(req, res) {
     // obj.directory.file -> app.config.database()
-    var connection = app.config.database();
-    var post_model = app.app.models.post_model;
+    var connection = application.config.database();
+    var post_model = application.app.models.post_model;
 
-    post_model.getPosts(connection, function(error, result) {
+    post_model.getPost(connection, function(error, result) {
       res.render('post/post', { post: result })
     });
 
