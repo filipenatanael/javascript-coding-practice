8e7f8602e569c7b3f4135270c95b563b90f07ab7
Filipe Natanael <filipenatanael@live.com>
2018-07-24 23:59:58 -0300

    Fixed getPosts

--- Node JS/app/models/post_model.js
+++ Node JS/app/models/post_model.js
@@ -1,5 +1,7 @@
 module.exports = function() {
-  this.getPosts(connection, callback){
+
+  this.getPosts = function(connection, callback) {
     connection.query('select * from news where id_news = 3', callback);
   }
+  return this;
 }
