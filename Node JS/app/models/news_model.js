410fb3a2f612d2271dd24dcea05a6e70f420a17c
Filipe Natanael <filipenatanael@live.com>
2018-07-27 01:04:42 -0300

    39 - Inserting records in the MySQL database

--- Node JS/app/models/news_model.js
+++ Node JS/app/models/news_model.js
@@ -1,12 +1,17 @@
 module.exports = function() {
-  /* get a all news available */
-  this.getAllNews = function(connection, callback) {
+  /* Get a all news available */
+  this.getAll = function(connection, callback) {
     connection.query('select * from news', callback)
   }
 
-  /* get a specific news available */
-  this.getSpecificNews = function(connection, callback) {
+  /* Get a specific news available */
+  this.getSpecific = function(connection, callback) {
     connection.query('select * from news where id_news = 3', callback)
+  }
+
+  /* Save a news on database */
+  this.save = function(news, connection, callback){
+    connection.query('insert into news set ?', news, callback)
   }
 
   return this;
--- Node JS/app/routes/news.js
+++ Node JS/app/routes/news.js
@@ -4,36 +4,20 @@ module.exports = function(application) {
     var connection = application.config.database();
     var news_model = application.app.models.news_model;
 
-    post_model.getAllNews(connection, function(error, result) {
-      res.render('news/index', { post: result })
+    news_model.getAll(connection, function(error, result) {
+      res.render('news/index', { news: result })
     });
-    // connection.query('select * from news', function(error, result){
-    //   res.render('news/index', { news: result });
-    // });
   });
 
   application.post('/news/store', function(req, res){
-    let connection app.config.database();
+    let news = req.body;
+    let connection = application.config.database();
     let news_model = application.app.models.news_model;
 
-    news_model.
-
-    let news = req.body;
-    res.send(news);
-  });
-
-}
-
-
-module.exports = function(application) {
-  application.get('/post', function(req, res) {
-    // obj.directory.file -> app.config.database()
-    var connection = application.config.database();
-    var post_model = application.app.models.post_model;
-
-    post_model.getPost(connection, function(error, result) {
-      res.render('post/post', { post: result })
+    news_model.save(news, connection, function(error, result){
+      res.redirect('/news/index');
+      //res.render('/news/index', { news: result });
     });
-
   });
+
 }
