9116198de9e589dd6a5e06f95524afa3c2d066ab
Filipe Natanael <filipenatanael@live.com>
2018-08-25 21:01:44 -0300

    Create a function to minify CSS

--- CSS-Optimization-With-Gulp/CSSMinifier/gulpfile.js
+++ CSS-Optimization-With-Gulp/CSSMinifier/gulpfile.js
@@ -1,10 +1,13 @@
 const Gulp = require("gulp");
 const GulpMinifyCss = require("gulp-minify-css");
 
+const CSS_DIRECTORY = "assets/css/**/*.css";
+const DIST_CSS = "public/assets/css"
+
 Gulp.task("default", function() {
   console.log("This is a default task!");
 });
 
 Gulp.task("minifyCss", function() {
-  console.log("This a minifyCss task!");
+  Gulp.src(CSS_DIRECTORY).pipe(GulpMinifyCss()).pipe(Gulp.dest(DIST_CSS));
 })
