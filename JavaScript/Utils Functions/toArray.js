ba3b34ef074bb9c6e624e8bc0ee612c7803a7622
Filipe Natanael <filipenatanael@live.com>
2018-09-26 23:02:36 -0300

    Convert an Array

---
+++ JavaScript/Utils Functions/toArray.js
@@ -0,0 +1,11 @@
+// Convert an Array like object to real Array:
+
+function toArray (list, start) {
+  start = start || 0;
+  var i = list.length - start;
+  var ret = new Array(i);
+  while (i--) {
+    ret[i] = list[i + start];
+  }
+  return ret
+}
