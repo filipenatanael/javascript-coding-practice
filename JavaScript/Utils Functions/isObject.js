53449184367aa66e1f49c6a3c41b4234e43d367a
Filipe Natanael <filipenatanael@live.com>
2018-09-26 22:41:00 -0300

    Get the raw type string of a value [object Object]

---
+++ JavaScript/Utils Functions/isObject.js
@@ -0,0 +1,8 @@
+/**
+ * Quick object check - this is primarily used to tell
+ * Objects from primitive values when we know the value
+ * is a JSON-compliant type.
+ */
+function isObject (obj) {
+  return obj !== null && typeof obj === 'object'
+}
