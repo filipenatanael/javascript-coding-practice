ccadd04aa84398c80d2ba5d83f50a1fe6341b5af
Filipe Natanael <filipenatanael@live.com>
2018-09-26 23:05:54 -0300

    Updated functions

--- JavaScript/Utils Functions/isPrimitive.js
+++ JavaScript/Utils Functions/isPrimitive.js
@@ -1,12 +1,10 @@
-/**
-* Check if value is primitive
-*/
+// Check if value is primitive
+
 function isPrimitive (value) {
   return (
     typeof value === 'string' ||
-    typeof value === 'number' ||
-    // $flow-disable-line
     typeof value === 'symbol' ||
+    typeof value === 'number' ||
     typeof value === 'boolean'
   )
 }
