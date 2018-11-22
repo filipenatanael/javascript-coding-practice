de6bcb5e4a86280b1e05a96ee7cdd8b94a1d3f10
Filipe Natanael <filipenatanael@live.com>
2018-11-21 23:10:49 -0200

    feat: redux on pratice

--- Redux/redux.js
+++ Redux/redux.js
@@ -1,3 +1,7 @@
+function generateId () {
+  return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
+}
+
 // Library Code
 function createStore (reducer) {
   // The store should have four parts
@@ -5,62 +9,75 @@ function createStore (reducer) {
   // 2. Get the state.
   // 3. Listen to changes on the state.
   // 4. Update the state
-   let state
+  let state
   let listeners = []
-   const getState = () => state
-   const subscribe = (listener) => {
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
     listeners.push(listener)
+    // Way to unsubscribe
     return () => {
       listeners = listeners.filter((l) => l !== listener)
     }
   }
-   const dispatch = (action) => {
+
+  const dispatch = (action) => {
+    console.log('dispatch was called.', action);
     state = reducer(state, action)
     listeners.forEach((listener) => listener())
   }
-   return {
+
+  return {
     getState,
     subscribe,
     dispatch,
   }
 }
- // App Code
+
+// App Code
 const ADD_TODO = 'ADD_TODO'
 const REMOVE_TODO = 'REMOVE_TODO'
 const TOGGLE_TODO = 'TOGGLE_TODO'
 const ADD_GOAL = 'ADD_GOAL'
 const REMOVE_GOAL = 'REMOVE_GOAL'
- function addTodoAction (todo) {
+
+function addTodoAction (todo) {
   return {
     type: ADD_TODO,
     todo,
   }
 }
- function removeTodoAction (id) {
+
+function removeTodoAction (id) {
   return {
     type: REMOVE_TODO,
     id,
   }
 }
- function toggleTodoAction (id) {
+
+function toggleTodoAction (id) {
   return {
     type: TOGGLE_TODO,
     id,
   }
 }
- function addGoalAction (goal) {
+
+function addGoalAction (goal) {
   return {
     type: ADD_GOAL,
     goal,
   }
 }
- function removeGoalAction (id) {
+function removeGoalAction (id) {
   return {
     type: REMOVE_GOAL,
     id,
   }
 }
- function todos (state = [], action) {
+
+// Reducers
+function todos (state = [], action) {
   switch(action.type) {
     case ADD_TODO :
       return state.concat([action.todo])
@@ -68,54 +85,117 @@ const REMOVE_GOAL = 'REMOVE_GOAL'
       return state.filter((todo) => todo.id !== action.id)
     case TOGGLE_TODO :
       return state.map((todo) => todo.id !== action.id ? todo :
-        Object.assign({}, todo, { complete: !todo.complete }))
+      Object.assign({}, todo, { complete: !todo.complete }))
     default :
       return state
   }
 }
- function goals (state = [], action) {
+
+function goals (state = [], action) {
   switch(action.type) {
     case ADD_GOAL :
-      return state.concat([action.goal])
+    return state.concat([action.goal])
     case REMOVE_GOAL :
-      return state.filter((goal) => goal.id !== action.id)
+    return state.filter((goal) => goal.id !== action.id)
     default :
-      return state
+    return state
   }
 }
- function app (state = {}, action) {
+
+// Combine Reducers
+function app (state = {}, action) {
   return {
     todos: todos(state.todos, action),
     goals: goals(state.goals, action),
   }
 }
- const store = createStore(app)
- store.subscribe(() => {
-  console.log('The new state is: ', store.getState())
+
+// Store
+const store = createStore(app)
+
+// Subscribe
+store.subscribe(() => {
+  const { goals, todos } = store.getState()
+  document.getElementById('goals').innerHTML = ''
+  document.getElementById('todos').innerHTML = ''
+
+  goals.forEach(addGoalToDOM)
+  todos.forEach(addTodoToDOM)
 })
- store.dispatch(addTodoAction({
-  id: 0,
-  name: 'Walk the dog',
-  complete: false,
-}))
- store.dispatch(addTodoAction({
-  id: 1,
-  name: 'Wash the car',
-  complete: false,
-}))
- store.dispatch(addTodoAction({
-  id: 2,
-  name: 'Go to the gym',
-  complete: true,
-}))
- store.dispatch(removeTodoAction(1))
- store.dispatch(toggleTodoAction(0))
- store.dispatch(addGoalAction({
-  id: 0,
-  name: 'Learn Redux'
-}))
- store.dispatch(addGoalAction({
-  id: 1,
-  name: 'Lose 20 pounds'
-}))
- store.dispatch(removeGoalAction(0))
+
+// UnSubscribe
+const UnSubscribe = store.subscribe(() => {
+  console.log('The store changed.');
+})
+
+/*
+   This method will extract the information from the input field,
+   reset the input field, and then dispatch an addTodoAction Action Creator
+   with the text that the user typed into the input field.
+*/
+function addTodo () {
+  console.log('addTodo() called by listener.');
+
+  const input = document.getElementById('todo')
+  const name = input.value
+  input.value = ''
+
+  store.dispatch(addTodoAction({
+    name,
+    complete: false,
+    id: generateId()
+  }))
+}
+
+function addGoal () {
+  const input = document.getElementById('goal')
+  const name = input.value
+  input.value = ''
+  store.dispatch(addGoalAction({
+    id: generateId(),
+    name,
+  }))
+}
+
+document.getElementById('todoBtn').addEventListener('click', addTodo)
+document.getElementById('goalBtn').addEventListener('click', addGoal)
+
+/* Create button to remove */
+function createRemoveButton (onClick) {
+  const removeBtn = document.createElement('button')
+  removeBtn.innerHTML = 'X'
+  removeBtn.addEventListener('click', onClick)
+  return removeBtn
+}
+
+/* Add element to the DOM when  subscribe*/
+function addTodoToDOM (todo) {
+  const node = document.createElement('li')
+  const text = document.createTextNode(todo.name)
+
+  const removeBtn = createRemoveButton(() => {
+    store.dispatch(removeTodoAction(todo.id))
+  })
+
+  node.appendChild(text)
+  node.appendChild(removeBtn)
+  node.style.textDecoration = todo.complete ? 'line-through' : 'none'
+
+  node.addEventListener('click', () => {
+    store.dispatch(toggleTodoAction(todo.id))
+  })
+
+  document.getElementById('todos').appendChild(node)
+}
+function addGoalToDOM (goal) {
+  const node = document.createElement('li')
+  const text = document.createTextNode(goal.name)
+
+  const removeBtn = createRemoveButton(() => {
+    store.dispatch(removeGoalAction(goal.id))
+  })
+
+  node.appendChild(text)
+  node.appendChild(removeBtn)
+  document.getElementById('goals').append(node)
+}
