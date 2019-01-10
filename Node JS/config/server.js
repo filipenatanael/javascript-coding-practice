3a48fb01f1d1d44f9f60909cd8cd5c2fb9ba9fa3
Filipe Natanael <filipenatanael@live.com>
2019-01-09 23:53:58 -0200

    feat: create database connection

--- Node JS/config/server.js
+++ Node JS/config/server.js
@@ -5,10 +5,13 @@ var bodyParser = require('body-parser');
 var app = express();
 
 /* Define constants */
-const VIEWS_DIRECTORY = './app/views';
-const ROUTES_DIRECTORY = 'app/routes';
-const MODALS_DIRECTORY = 'app/models';
-const DATABASE_CONFIG = 'config/database.js';
+const VIEWS_DIRECTORY = 'src/views';
+const ROUTES_DIRECTORY = 'src/routes';
+const MODALS_DIRECTORY = 'src/models';
+const DATABASE_CONFIG = 'database/createConnection.js';
+
+/* <%= EJS %> is a visualization engine, with it we can easily and simply transport data from the backend to the front end,
+we basically managed to use javascript codes in the html of our pages. */
 
 app.set('view engine', 'ejs');  // Set express engine to use EJS
 app.set('views', VIEWS_DIRECTORY) // Set default directory to express
@@ -16,6 +19,9 @@ app.set('views', VIEWS_DIRECTORY) // Set default directory to express
 /* Works like a Middleware */
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+/* Consign makes applications easier to develop with logical file separation and automatic script loading.
+Consign can be used to autoload models, routes, schemas, configs, controllers, object maps... etc... */
 consign()
     .include(ROUTES_DIRECTORY)
     .then(DATABASE_CONFIG)
