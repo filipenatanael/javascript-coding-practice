e2069594fa3da92d58218ce1abdbc89ebe110105
Filipe Natanael <filipenatanael@live.com>
2019-03-13 22:26:57 -0300

    feat: create a simple node application

---
+++ nodejs-babel-es6-async-await/src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import express from 'express';
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('Starting...');
+});
+
+
+export default app;
